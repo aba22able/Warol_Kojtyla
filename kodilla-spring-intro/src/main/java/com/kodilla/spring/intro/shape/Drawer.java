abbabb09919671940182405eaa2b8c2a524bd798
Albert Heesch <aheesch@op.pl>
2022-05-20 19:55:49 +0200

    gradle.build run faile

--- kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
@@ -1,14 +1,14 @@
 package com.kodilla.spring.intro.shape;
 
 public class Drawer {
-    public void doDrawing() {
 
-        Figure figure;
+    final Shape shape;
 
-        figure = new Circle();
-        figure.draw();
+    public Drawer(final Shape shape) {
+        this.shape = shape;
+    }
 
-        figure = new Triangle();
-        figure.draw();
+    public String doDrawing() {
+        return shape.draw();
     }
 }

22384317c960e19247ca7b335ff27f97d1a863df
Albert Heesch <aheesch@op.pl>
2022-05-31 00:16:39 +0200

    15.1 LoggerTestSuite

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -1,4 +1,26 @@
 package com.kodilla.patterns.singleton;
 
-public class LoggerTestSuite {
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoggerTestSuite
+{
+    private static Logger logger;
+
+    @BeforeAll
+    public static void openSettingsFile() {
+        logger = Logger.INSTANCE;
+    }
+
+    @Test
+    void testGetFileName() {
+        //Given
+        //When
+        logger.log("new log");
+        String logName = logger.getLastLog();
+        //Then
+        assertEquals("new log", logName);
+    }
 }

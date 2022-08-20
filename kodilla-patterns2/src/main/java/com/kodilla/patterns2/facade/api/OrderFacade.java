d34762a8bd7bc586bc1a6cf98309369eb353ca45
Albert Heesch <aheesch@op.pl>
2022-08-20 16:39:43 +0200

    24.3 processOrder method LOGGER

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
@@ -11,8 +11,8 @@ import java.math.BigDecimal;
 @Service
 public class OrderFacade {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacade.class);
-    private final ShopService shopService;
+    private static Logger LOGGER = LoggerFactory.getLogger(OrderFacade.class);
+    private ShopService shopService;
 
     @Autowired
     public OrderFacade(ShopService shopService) {
